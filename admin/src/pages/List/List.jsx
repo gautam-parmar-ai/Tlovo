9482875bc12904bb75e81dd3afe21a948551bce0
Gautam Parmar <gautamparmarverona@gmail.com>
2026-05-11 22:36:25 +0200

    List changes

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -1,3 +1,10 @@
+import React from "react";
+import "./List.css";
+import { assets } from "../../assets/assets";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+
 const List = ({ url }) => {
   const [list, setList] = useState([]);
 
