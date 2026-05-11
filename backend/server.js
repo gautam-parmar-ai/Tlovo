f3d93b5206a49fd94e2a88b6e4451eb2a92b99fa
Gautam Parmar <gautamparmarverona@gmail.com>
2026-05-11 19:57:53 +0200

    up

--- backend/server.js
+++ backend/server.js
@@ -5,27 +5,26 @@ import cors from "cors";
 import { connectDB } from "./config/db.js";
 import foodRouter from "./routes/foodRoutes.js";
 import userRouter from "./routes/userRoutes.js";
-
 import cartRouter from "./routes/cartRoute.js";
 import orderRouter from "./routes/orderRoute.js";
-//app config
+
+// app config
 const app = express();
 const port = 4000;
 
-//middleware
+// middleware
 app.use(express.json());
 app.use(cors());
 
-//DB Connection
-
+// DB Connection
 connectDB();
 
-//api endpoints
+// api endpoints
 app.use("/api/food", foodRouter);
 app.use("/images", express.static("uploads"));
 app.use("/api/user", userRouter);
 app.use("/api/cart", cartRouter);
-app.use("/api/order/", orderRouter);
+app.use("/api/order", orderRouter);
 
 app.get("/", (req, res) => {
   res.send("API WORKING");
