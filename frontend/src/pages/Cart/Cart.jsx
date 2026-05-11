92c48d8ff581064834e7cd4c5dbc876edf8e2e20
Gautam Parmar <gautamparmarverona@gmail.com>
2026-05-11 22:22:44 +0200

    cart chagnes

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -28,7 +28,8 @@ const Cart = () => {
                 <div>
                   {" "}
                   <div className="cart-items-title cart-items-item">
-                    <img src={url + "/images/" + item.image} alt="" />
+                    <img className="food-item-image" src={item.image} />
+
                     <p> {item.name} </p>
                     <p> € {item.price} </p>
                     <p> {cartItems[item._id]} </p>
